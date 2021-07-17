19fa29fb1d6918633bed8762f57a451c8ee76e74
Nelson Frank <36732037+Nelsonfrank@users.noreply.github.com>
2021-07-17 19:37:08 +0000

    feat: Add tailwindcss and setup

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todos from './todos';
+
+const App =()=> {
+  return (
+    <Todos />
+  );
+}
+
+export default App;
---
+++ src/app/todos/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todos = ()=>{
+  return(
+    <section>
+        <p className="text-3xl text-blue-300">Hello, world</p>
+    </section>
+  )
+}
+
+export default Todos;
